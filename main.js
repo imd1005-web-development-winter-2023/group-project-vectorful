f0cbdc08c022d41a5e87dd7b5d3da7187fc517e4
sumaiyaibrahim04 <125154848+sumaiyaibrahim04@users.noreply.github.com>
2023-04-09 16:09:54 -0400

    Added play again option

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
 //letters 
 const letters ="abcdefghijklmnopqrstuvwxyz";
 
-//get array from letters 
-let lettersArray = Array.from(letters);
-
-// select letters container
-let lettersContainer = document.querySelector(".letters");
-
-//generate letters 
-lettersArray.forEach(letter => {
- 
-    //create span
-    let span = document.createElement("span");
-
-    // create letters text node 
-    let theLetter = document.createTextNode(letter);
-
-    //append the letter to span
-    span.appendChild(theLetter);
-
-    // add class on span
-    span.className="letter-box";
-
-    // append doan to the letters container
-    lettersContainer.appendChild(span);
-});
-
+let lettersArray;
+let lettersContainer;
+let allKeys;
+let randomPropNum;
 
 //object of words and themes
 const words ={
@@ -34,90 +13,148 @@ const words ={
     superheros:["Superman","Spider Man", "Batman", "Captain America", "Iron Man", "The Flash", "Moon Knight", "Blue Beetle", "Daredevil", "Dr Strange", "Robin", "Ant Man", "Hulk", "Wolverine", "Aquaman", "Hawkeye", "Cyborg", "Beast Boy", "Deadpool", "Shazam", "Starfire", "Raven", "Supergirl"]
 };
 
-//get random word or property 
-
-let allKeys = Object.keys(words);
-
-//random number depending on keys length
-let randomPropNum = Math.floor(Math.random() * allKeys.length)//0, 1, 2 or 3
-
-//allKeys[0]=movies
-//allKeys[1]=countries
-//allKeys[2]=superheros
-//allKeys[3]=bands
-
-// theme
-let randomPropName = allKeys[randomPropNum];
-
-// theme words
-let randomPropValue = words[randomPropName];
-
-// random number depending on words
-let randomValueNum = Math.floor(Math.random() * randomPropValue.length);
-
-// the chosen word
-let randomValueValue = randomPropValue[randomValueNum];
-
-//set theme info
-document.querySelector(".game-info .theme span"). innerHTML = ' '+ randomPropName + ' '+randomValueValue;
-
-//select letters guess element or container
-let lettersGuessContainer = document.querySelector(".letters-guess");
-
-//convert chosen word to array of characters/letters
-let letterAndSpace = Array.from(randomValueValue);
-
-// create spans depending on word
-letterAndSpace.forEach(letter => {
-
-    // create empty span 
-    let emptySpan = document.createElement("span");
-
-    //if letter is space
-    if (letter === ' '){
-
-        //add class to the span
-        emptySpan.className ='with-space';
-
-    }
-
-    // append spans to the letters guess container
-    lettersGuessContainer.appendChild(emptySpan);
+let randomPropName;
+let randomPropValue;
+let randomValueNum;
+let randomValueValue;
+let correctAttempts;
+let livesLeft;
+let span;
+let theLetter;
+let lettersGuessContainer;
+let letterAndSpace;
+let guessSpans;
+let theDrawing;
+let theStatus;
+let clickedLetter;
+let chosenWord;
+let failSnd;
+let successSnd;
+let uniqueChars;
+let i;
+let div;
+let divText;
+let restartButton;
+let popup;
+ 
+// call initialize to start the game
+initialize();
 
 
-});
+function initialize(){
 
-// select guess spans
-let guessSpans = document.querySelectorAll(".letters-guess span")
+    // set img shown to default img
+    startImg();
+    
+    //get array from letters 
+    lettersArray = Array.from(letters);
+
+    // select letters container
+    lettersContainer = document.querySelector(".letters");
+
+    //remove finished class from letters container
+    lettersContainer.classList.remove("finished");
+
+    //get random word or property 
+    allKeys = Object.keys(words);
+
+    //random number depending on keys length
+    randomPropNum = Math.floor(Math.random() * allKeys.length)//0, 1, 2 or 3
+
+    //allKeys[0]=movies
+    //allKeys[1]=countries
+    //allKeys[2]=superheros
+
+    // theme
+    randomPropName = allKeys[randomPropNum];
+
+    // theme words
+    randomPropValue = words[randomPropName];
+
+    // random number depending on words
+    randomValueNum = Math.floor(Math.random() * randomPropValue.length);
+
+    // the chosen word
+    randomValueValue = randomPropValue[randomValueNum];
+
+    // set correct attempts
+    correctAttempts = 0;
+
+    // set lives left
+    livesLeft = 10;
 
 
-// set lives left
-let livesLeft = 10;
+    lettersArray.forEach(letter => {
+ 
+        //create span
+        span = document.createElement("span");
 
-// set correct attempts
-let correctAttempts = 0;
+        // create letters text node 
+        theLetter = document.createTextNode(letter);
 
+        //append the letter to span
+        span.appendChild(theLetter);
 
+        // add class on span
+        span.className="letter-box";
 
-//select the draw element
-let theDrawing = document.querySelector(".hangman-draw");
+        // append span to the letters container
+        lettersContainer.appendChild(span);
+    });
+
+    // set theme info
+    document.querySelector(".game-info .theme span"). innerHTML = ' '+ randomPropName + ' '+randomValueValue;
+
+    // select letters guess element or container
+    lettersGuessContainer = document.querySelector(".letters-guess");
+
+    // convert chosen word to array of characters/letters
+    letterAndSpace = Array.from(randomValueValue);
+
+    // create spans depending on word
+    letterAndSpace.forEach(letter => {
+
+        // create empty span 
+        emptySpan = document.createElement("span");
+
+        //if letter is space
+        if (letter === ' '){
+
+            //add class to the span
+            emptySpan.className ='with-space';
+
+        }
+
+        // append spans to the letters guess container
+        lettersGuessContainer.appendChild(emptySpan);
+
+    });
+
+    // select guess spans
+    guessSpans = document.querySelectorAll(".letters-guess span");
+
+    // select the draw element
+    theDrawing = document.querySelector(".hangman-draw");
+    
+}
+
 
 
 // handles clicking on letters 
 document.addEventListener("click", (e) =>{
 
     // set the choice status
-    let theStatus = false;
+    theStatus = false;
    
     if(e.target.className === 'letter-box'){
 
         e.target.classList.add("clicked");
 
         // get clicked letter 
-        let clickedLetter = e.target.innerHTML.toLowerCase();
+        clickedLetter = e.target.innerHTML.toLowerCase();
 
         // the chosen word 
-        let chosenWord = Array.from(randomValueValue.toLowerCase());
+        chosenWord = Array.from(randomValueValue.toLowerCase());
 
 
 
@@ -167,27 +204,25 @@ document.addEventListener("click", (e) =>{
             }
 
             // add class wrong on the drawing element
-
             theDrawing.classList.add(`wrong-${livesLeft}`);
        
             //play fail sound
-            let failSnd = document.getElementById("fail");
+            failSnd = document.getElementById("fail");
             failSnd.currentTime = 0.001;
             failSnd.play();
 
+            // if no lives are left user 
             if (livesLeft === 0) {
 
                 loseImg();
                 endGame();
-
-                lettersContainer.classList.add("finished");
 
             }
 
         } else {
 
             // play success sound
-            let successSnd = document.getElementById("success");
+            successSnd = document.getElementById("success");
             successSnd.currentTime = 0.001;
             successSnd.play();
 
@@ -195,23 +230,24 @@ document.addEventListener("click", (e) =>{
             correctAttempts++;
 
             // filter out duplicate letters from chosen word
-            let uniqueChars = chosenWord.filter((c, index)=> {
+            uniqueChars = chosenWord.filter((c, index)=> {
                 return chosenWord.indexOf(c)===index;
             });
 
             // remove space from chosen word
-            for ( let i = 0; i < uniqueChars.length; i++ ){
+            for ( i = 0; i < uniqueChars.length; i++ ){
 
                 if ( uniqueChars[i] === ' ' ){
                     uniqueChars.length--;
                 }
             }
 
+            // 
             if (correctAttempts === uniqueChars.length){
                 
                 winImg();
                 winGame();
-                lettersContainer.classList.add("finished");
+                
             }
         }
     }
@@ -219,17 +255,19 @@ document.addEventListener("click", (e) =>{
 });
 
 // end game function 
-
 function endGame(){
 
-    //play game over audio
+    //display finished lettersContainer
+    lettersContainer.classList.add("finished");
+
+    // play game over audio
     document.getElementById("game-over").play();
 
     // create popup div
-    let div = document.createElement("div");
+    div = document.createElement("div");
 
     // create text 
-    let divText = document.createTextNode(`Game over! The word was ${randomValueValue}`);
+    divText = document.createTextNode(`Game over! The word was ${randomValueValue}`);
 
     // append text to div
     div.appendChild(divText);
@@ -237,21 +275,39 @@ function endGame(){
     // add class on div
     div.className = 'popup';
 
-    //append to the body
+    // create restart button
+    restartButton = document.createElement("button");
+
+    // add inner text to button
+    restartButton.innerText= 'Play Again';
+
+    // append restartButton to div
+    div.appendChild(restartButton);
+
+    // append div to the body
     document.body.appendChild(div);
+
+    // run initialize and removeStuff when button is clicked
+    restartButton.addEventListener('click',() => {    
+        removeStuff();
+        initialize();   
+   });
 
 }
 
 function winGame(){
 
+    //display finished lettersContainer
+    lettersContainer.classList.add("finished");
+
     //play winning audio
     document.getElementById("win").play();
 
     // create popup div
-    let div = document.createElement("div");
+    div = document.createElement("div");
 
     // create text 
-    let divText = document.createTextNode(`You won! The word was ${randomValueValue}`);
+    divText = document.createTextNode(`You won! The word was ${randomValueValue}`);
 
     // append text to div
     div.appendChild(divText);
@@ -259,10 +315,48 @@ function winGame(){
     // add class on div
     div.className = 'popup';
 
-    //append to the body
+    // create restart button
+    restartButton = document.createElement("button");
+
+    // add inner text to button
+    restartButton.innerText= 'Play Again';
+
+    // append restartButton to div
+    div.appendChild(restartButton);
+
+    // append div to the body
     document.body.appendChild(div);
 
+
+    // run initialize and removeStuff when button is clicked
+    restartButton.addEventListener('click',() => {    
+        removeStuff();
+        initialize();   
+   });
+
 }
+
+function removeStuff(){
+
+    // remove popup
+    popup = document.querySelector(".popup");
+    popup.remove();
+
+    // remove lettersContainer children
+    while (lettersContainer.firstChild) {
+        lettersContainer.removeChild(lettersContainer.firstChild);
+      }
+
+    // remove lettersGuessContainer children
+    while(lettersGuessContainer.firstChild){
+        lettersGuessContainer.removeChild(lettersGuessContainer.firstChild);
+    }
+
+}
+
+function startImg() {
+    document.getElementById("manny-start").src="images/Super-Manny_1.png";
+  }
 
 function wrongAttemptImg1() {
     document.getElementById("manny-start").src="images/Super-Manny_2.png";
